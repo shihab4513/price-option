23d0feb665852bb732bc93154eed8fe5548ac517
shihab4513 <shihab4513.uiu@gmail.com>
2024-12-06 11:26:04 +0600

    toggle function added for icon i.e open and close

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import PropTypes from 'prop-types';
 const Link = ({route}) => {
   return (
     <li className='mr-8'>
             <a href={route.path}>{route.name}</a></li>
   )
+}
+
+Link.PropTypes={
+    route:PropTypes.object
 }
 
 export default Link
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from '../Link/Link';
+import { AiOutlineMenu,AiOutlineClose} from "react-icons/ai";
 
 const Nav = () => {
+    const [open,setOpen]=useState(false);
     const routes = [
         { id: 1, name: "Home", path: "/" },
         { id: 2, name: "About", path: "/about" },
@@ -14,7 +16,14 @@ const Nav = () => {
       
   
     return (
-     <nav>
+     <nav className='md:hidden text-2xl'>
+        <div onClick={()=>{setOpen(!open)}}>
+            {
+                open===true ? <AiOutlineClose></AiOutlineClose>:<AiOutlineMenu className=''></AiOutlineMenu>
+            }
+        
+        </div>
+        
         <ul className='md:flex'>
         {
         routes.map(route=><Link route={route} key={route.id}></Link>)
